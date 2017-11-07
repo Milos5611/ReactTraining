9cf6c2b40d36628d9446c8331f6410c2d6db676a
Milos Nikolic <milos.nikolic@prodyna.com>
2017-11-07 23:45:24 +0100

    Welcome component

--- src/app/app.js
+++ src/app/app.js
@@ -4,16 +4,12 @@ import {Route, Router, Switch} from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import {Provider} from "react-redux";
 import React from "react";
-import {addLocaleData} from "react-intl";
 import customHistory from "./common/history";
-import de from "react-intl/locale-data/de";
 import domready from "domready";
-import en from "react-intl/locale-data/en";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import {render} from "react-dom";
+import Welcome from "../app/component/Welcome";
 import store from "./common/store";
-
-addLocaleData([...de, ...en]);
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -28,6 +24,7 @@ const MainApplication = () => {
                     <Route
                         exact
                         path={ROUTES.HOME}
+                        component={Welcome}
                     />
                 </Switch>
             </div>
